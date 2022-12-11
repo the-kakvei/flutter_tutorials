be9a6dd7d874e73698bac54a4bfa64ddbc4edc5f
Kakada <nkakada@outlook.com>
2022-12-11 20:33:47 +0700

    show image in a list

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show get;
 import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
 class App extends StatefulWidget {
   createState() {
@@ -14,13 +15,18 @@ class App extends StatefulWidget {
 
 class AppState extends State<App> {
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fetchImage() async {
     counter += 1;
+
     var url = Uri.https('jsonplaceholder.typicode.com', '/photos/$counter');
     var response = await get(url);
     var imageModel = ImageModel.fromJson(json.decode(response.body));
-    inspect(imageModel);
+
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(BuildContext context) {
@@ -33,7 +39,7 @@ class AppState extends State<App> {
           onPressed: fetchImage,
           child: Icon(Icons.update),
         ),
-        body: Text('$counter button have pressed'),
+        body: ImageList(images),
       ),
     );
   }
