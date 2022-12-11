f0bbadbd82c9b99c1837ec37041e207893c5d1cc
Kakada <nkakada@outlook.com>
2022-12-11 12:42:56 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-// 1. Import a helper library to get content on screen
 import 'package:flutter/material.dart';
 
-// 2. Define a main function to run when our app start
 void main() {
-// 3. Inside that main function, create a new text widget to show text
   var app = MaterialApp(
-    // MaterialApp is a core widget
-    // home is a named parameter, the first thing to show up
-    home: Text('Hi There!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic Images'),
+      ),
+    ),
   );
 
-// 4. Take that widget and put it on the screen
   runApp(app);
 }
